74534925c6616141a217870177a2bdbf57c8cda3
Andrew Stucki <andrew.stucki@redpanda.com>
2025-11-26 19:30:24 -0500

    fix logger

--- multicluster/raft.go
+++ multicluster/raft.go
@@ -334,54 +334,44 @@ type raftLogr struct {
 }
 
 func (r *raftLogr) Debug(v ...any) {
-	r.logger.V(1).Info("DEBUG", v...)
+	r.logger.V(1).Info(fmt.Sprintf("[DEBUG] %s", fmt.Sprint(v...)))
 }
+
 func (r *raftLogr) Debugf(format string, v ...any) {
-	if format == "" {
-		r.logger.V(0).Info("DEBUG", v...)
-	} else {
-		r.logger.V(0).Info(fmt.Sprintf("[DEBUG] %s", fmt.Sprintf(format, v...)))
-	}
+	r.logger.V(1).Info(fmt.Sprintf("[DEBUG] %s", fmt.Sprintf(format, v...)))
 }
 
 func (r *raftLogr) Error(v ...any) {
-	r.logger.Error(errors.New("an error occurred"), "ERROR", v...)
+	text := fmt.Sprint(v...)
+	r.logger.Error(errors.New(text), text)
 }
+
 func (r *raftLogr) Errorf(format string, v ...any) {
-	if format == "" {
-		r.logger.Error(errors.New("an error occurred"), "ERROR", v...)
-	} else {
-		text := fmt.Sprintf(format, v...)
-		r.logger.Error(errors.New(text), text)
-	}
+	text := fmt.Sprintf(format, v...)
+	r.logger.Error(errors.New(text), text)
 }
 
 func (r *raftLogr) Info(v ...any) {
-	r.logger.V(0).Info("INFO", v...)
+	r.logger.V(0).Info(fmt.Sprintf("[INFO] %s", fmt.Sprint(v...)))
 }
+
 func (r *raftLogr) Infof(format string, v ...any) {
-	if format == "" {
-		r.logger.V(0).Info("INFO", v...)
-	} else {
-		r.logger.V(0).Info(fmt.Sprintf("[INFO] %s", fmt.Sprintf(format, v...)))
-	}
+	r.logger.V(0).Info(fmt.Sprintf("[INFO] %s", fmt.Sprintf(format, v...)))
 }
 
 func (r *raftLogr) Warning(v ...any) {
-	r.logger.V(0).Info("WARN", v...)
+	r.logger.V(0).Info(fmt.Sprintf("[WARN] %s", fmt.Sprint(v...)))
 }
+
 func (r *raftLogr) Warningf(format string, v ...any) {
-	if format == "" {
-		r.logger.V(0).Info("WARN", v...)
-	} else {
-		r.logger.V(0).Info(fmt.Sprintf("[WARN] %s", fmt.Sprintf(format, v...)))
-	}
+	r.logger.V(0).Info(fmt.Sprintf("[WARN] %s", fmt.Sprintf(format, v...)))
 }
 
 func (r *raftLogr) Fatal(v ...any) {
 	r.Error(v...)
 	os.Exit(1)
 }
+
 func (r *raftLogr) Fatalf(format string, v ...any) {
 	r.Errorf(format, v...)
 	os.Exit(1)
@@ -389,8 +379,9 @@ func (r *raftLogr) Fatalf(format string, v ...any) {
 
 func (r *raftLogr) Panic(v ...any) {
 	r.Error(v...)
-	panic("unexpected error")
+	panic(fmt.Sprint(v...))
 }
+
 func (r *raftLogr) Panicf(format string, v ...any) {
 	r.Errorf(format, v...)
 	panic(fmt.Sprintf(format, v...))
